35852c39a2465e8256b8a87616540b302ab0b320
Rayyan Hunerkar <Ray26318@gmail.com>
2020-05-22 23:22:28 +0530

    Fixed the inclusion of unnecessary Groups

--- csvmanip.py
+++ csvmanip.py
@@ -16,10 +16,8 @@ print("\nMerging the reports into one")
     
 master = pd.read_excel(masterlist)
 
-
 for i in range(noReports):
     report = pd.concat([pd.read_csv(reportlist[j]) for j in range(noReports)]) #the loop converts the report(s) into one single file
-
 
 masterColumn = master.columns.values.tolist() #Retrives the headers of the master list
 
@@ -31,7 +29,6 @@ if os.path.isdir(path):
     print('\nDirectory Already created')
 else:
     os.mkdir(path, mode = 0o777) #Makes a directory to store the seperated reports
-
 
 print("\nSearching for IP addresses for all Columns\n ")
 for i in range(len(masterColumn)):
--- pdfmanip.py
+++ pdfmanip.py
@@ -13,10 +13,16 @@ pages = []
 for i in range(pdfname.start_page(NumPages=tPages,object= initPDF), pdfname.end_page(NumPages=tPages,object = initPDF)):
     pages.append(i)    # retrives the pages from the pdf report
 
-print('\nConverting the report to csv')
+
+print('\nFetching Discovered Devices from PDF...')
+
 tb.convert_into(pdflist, 'test.csv', output_format='csv', pages=pages)
-                
+
+print('\nTemporarily saving the data in test.csv')
+
 pdfReport = pd.read_csv('test.csv',  encoding='unicode_escape')
+
+pdfReport = pdfReport.dropna(subset = ['Node'])
 
 master = pd.read_excel(masterlist)
 
@@ -26,21 +32,24 @@ masterColumn = master.columns.values.tolist() #Retrives the headers of the maste
 
 finalColumns = []
 convDF = []
-revisedMasterList = pd.read_csv('revisedMasterList.csv')
+# revisedMasterList = pd.read_csv('revisedMasterList.csv')
 
 # the loop fonds the columns corresponding with the IPs
 
 for i in range(len(masterColumn)):
     result = pdfReport[pdfReport['Node'].isin(master[masterColumn[i]])]
+    print(result.empty)
     if result.empty != True:
+        print(masterColumn[i])
         finalColumns.append(masterColumn[i]) 
-
+        
+        
 for j in range(len(finalColumns)):
     convDF.append(master[finalColumns[j]])
 convDF = pd.DataFrame(convDF)
 convDF1 = pd.DataFrame(convDF).stack()
 
-print('\nSearching for unscanned IPs')
+print('\nSearching for Unscanned IP Addresses...')
 IPdf = convDF1[~convDF1.isin(pdfReport['Node'])] #retrives the unscanned IPs
 columnGroup= []
 initialScanStatus = []
@@ -64,5 +73,9 @@ finalReport.columns = ['IP', 'Group','Initial Scan Status']
 
 print('\nCreating file')
 
-finalReport.to_csv('Initial_Scan_Status_Report.csv', index=False, header=True)
+resultFile = 'Scan_Status_Report ' + os.path.splitext(pdflist)[0].replace('PDF', '') + '.csv'
+
+hdr = False if os.path.exists(resultFile) else True
+
+finalReport.to_csv(resultFile, index=False, header=hdr, mode='a')
 os.remove('test.csv')
